646396b5692a2c7af7581393779870d7f504c058
Camille Anelli <camilleanelli32@gmail.com>
2025-12-16 17:31:55 +0100

    change roots

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,22 @@ import './index.css'
 import App from './App.jsx'
 import App2 from './App2.jsx'
 import App3 from './App3.jsx'
+import AppHook from './AppHook.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App3 />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App3 />
+    </StrictMode>
+  );
+}
+
+const hooksPageElement = document.getElementById('hooks-page');
+if (hooksPageElement) {
+  createRoot(hooksPageElement).render(
+    <StrictMode>
+      <AppHook />
+    </StrictMode>
+  );
+}
